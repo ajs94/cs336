78da6c68f8cc7261eb7975ffc0f3b57fa17d392f
Aaron Santucci <ajs94@gold12.cs.calvin.edu>
2018-11-16 20:20:50 -0500

    homework3

---
+++ homework3/public/scripts/getPerson.js
@@ -0,0 +1,32 @@
+/* Aaron Santucci
+ * CS 336
+ * Homework 2
+ * getPerson.js searching for Person by ID
+ */
+
+"use strict"
+
+console.log('getPerson.js running');
+
+$( document ).ready(function() {
+
+    $("form").on( "submit", function(event) {
+        event.preventDefault();
+        console.log("searching by ID");
+
+        $.ajax({
+            url: "/person/" + $("#id").val(),
+            type: "GET"
+        })
+        .done(function(result){
+            console.log("getPerson: AJAX request successful.");
+            $("#Result").html("<p>" + result.firstName + " " + result.lastName + ", " + result.startDate + "</p>");
+        })
+		.fail(function(xhr, status, errorThrown) {
+			console.log('getPerson: AJAX request failed');
+			$("#Result").html("<p>" + 'ID not found' + "</p>");
+		})
+    });
+
+
+});
